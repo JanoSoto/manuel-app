bce5e969509b90cab4a39f1f030b622374368385
danielVargas <daniel.vargas@usach.cl>
2017-05-22 14:30:17 -0400

    FIX agregado boton cerrar sesión + páginas de error personalizadas

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,11 @@
 class ErrorsController < ApplicationController
+  layout 'errors'
+
   def not_found
-  	render(:status => 404)
+  	render(:status => 404, :turbolinks => false)
   end
 
   def internal_server_error
-  	render(:status => 500)
+  	render(:status => 500, :turbolinks => false)
   end
 end
