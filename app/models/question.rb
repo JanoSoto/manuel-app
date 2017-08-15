06cd0c71479edb9452efab787ddf98def45a78de
janosoto <alejandro.soto@usach.cl>
2017-08-15 01:17:21 -0300

    REFACTOR Remoción de relaciones many-to-many

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,4 @@ class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :answer_options
 
-  has_many :survey_results
-  #has_many :answer_options, through: :survey_results
-  has_many :assigned_surverys, through: :survey_results
 end
