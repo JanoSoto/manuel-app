5a4ca58a120ac277d8ce5b38390effdbe97c0767
janosoto <alejandro.soto@usach.cl>
2017-08-23 12:44:19 -0300

    ADD Asignación de jefe de proyecto

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,16 @@
 module CoursesHelper
+	def get_role_class(role)
+		case role
+			when ''
+				return 'btn-default'
+			when nil
+				return 'btn-default'
+			when 'ayudante'
+				return 'btn-primary'
+			when 'jefe de grupo'
+				return 'btn-success'
+			when 'jefe de proyecto'
+				return 'btn-warning'
+		end
+	end
 end
