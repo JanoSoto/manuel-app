d53b2253f4fd4e71bf67914bdb6f979cc92be72b
janosoto <alejandro.soto@usach.cl>
2018-04-08 14:08:15 -0300

    ADD Asignación por defecto de estado activo para nuevos usuarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ActiveRecord::Base
   has_many :assigned_surveys
   has_many :courses, through: :assigned_surveys
 
+  before_save :set_defaults
+
   def admin?
   	return self.roles_id == 1
   end
@@ -59,4 +61,10 @@ class User < ActiveRecord::Base
   def notify_survey(survey)
     NotificationMailer.notify_survey(self, survey).deliver_now
   end
+
+  private 
+    def set_defaults
+      self.status = true
+    end
+
 end
